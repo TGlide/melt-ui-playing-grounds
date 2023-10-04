cdbe17f0d102b9eeebb5a7f897aeb2df652b09d3
tglide <26071571+TGlide@users.noreply.github.com>
2023-10-04 22:23:48 +0100

    binding select example

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,15 @@
+<script>
+	import Select from '../components/Select.svelte';
+</script>
+
 <h1 class="text-4xl font-bold">Hello!</h1>
+
+<div class="w-[300px]">
+	<Select
+		selected="Chocolate"
+		options={{
+			sweet: ['Caramel', 'Chocolate', 'Strawberry', 'Cookies & Cream'],
+			savory: ['Basil', 'Bacon', 'Rosemary']
+		}}
+	/>
+</div>
